6042d96e66f15d5b6c9da0eca960cc1a2ff25046
Vankka <vankka.main@gmail.com>
2024-07-05 23:59:01 +0300

    Update Gradle, Workflows, Shields

--- src/main/java/dev/vankka/mcdiscordreserializer/minecraft/MinecraftSerializer.java
+++ src/main/java/dev/vankka/mcdiscordreserializer/minecraft/MinecraftSerializer.java
@@ -1,6 +1,6 @@
 /*
  * MCDiscordReserializer: A library for transcoding between Minecraft and Discord.
- * Copyright (C) 2018-2023 Vankka
+ * Copyright (C) 2021-2024 Vankka
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
